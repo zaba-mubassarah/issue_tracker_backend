27b0906a2b10832d01b768e1ec13f420abbb133e
zaba <zaba.mubassarah@gmail.com>
2022-09-20 16:42:07 -1200

    updated

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ import bodyParser from "body-parser";
 import issueRoutes from "./routes/issues.js";
 import usersRoute from "./routes/users.js";
 const app = express();
+import cors from "cors";
 const PORT = 5000;
 app.use(bodyParser.json());
 
-app.use('/issues',issueRoutes);
-app.use('/users',usersRoute);
+app.use('/issues', cors(),issueRoutes);
+app.use('/users',cors(),usersRoute);
 app.get('/',(req,res)=>{
     res.send('Welcome to the backend world!');
 })
