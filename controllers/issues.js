6aef1178d5fc29acc7d20612374dee991513a0a4
zaba <zaba.mubassarah@gmail.com>
2022-09-22 18:02:19 -1200

    neweww

--- controllers/issues.js
+++ controllers/issues.js
@@ -1,34 +1,35 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 let issues = [];
-export const getIssues = ((req,res)=>{
-    res.send(issues);
-});
-export const getIssuesById =((req,res)=>{
-    const {id} = req.params;
-    const dataById = issues.find((item)=>item.id === id);
-    res.send(dataById);
-});
-export const saveIssues =((req,res)=>{
-    const request = req.body;
-    issues.push({...request, id: uuid()});
-    res.send(issues);
-});
+export const getIssues = (req, res) => {
+  res.send(issues);
+};
+export const getIssuesById = (req, res) => {
+  const { id } = req.params;
+  const dataById = issues.find((item) => item.id === id);
+  res.send(dataById);
+};
+export const saveIssues = (req, res) => {
+  console.log("req,", req);
+  const request = req.body;
+  issues.push({ ...request, id: uuid() });
+  res.send(issues);
+};
 
-export const deleteIssue =((req,res)=>{
-    const {id} = req.params;
-    
-    issues = issues.filter((item)=>{
-        return item.id !== id
-    });
-    
-    res.send(`issues of id ${id} is deleted`);
-});
+export const deleteIssue = (req, res) => {
+  const { id } = req.params;
 
-export const updateIssue =((req,res)=>{
-    const issue = issues.find((issue) => issue.id === req.params.id);
-    issue.title = req.body.title;
-    issue.description = req.body.description;
-    issue.module = req.body.module;
+  issues = issues.filter((item) => {
+    return item.id !== id;
+  });
 
-    res.send(issues);
-});
+  res.send(`issues of id ${id} is deleted`);
+};
+
+export const updateIssue = (req, res) => {
+  const issue = issues.find((issue) => issue.id === req.params.id);
+  issue.title = req.body.title;
+  issue.description = req.body.description;
+  issue.module = req.body.module;
+
+  res.send(issues);
+};
--- controllers/users.js
+++ controllers/users.js
@@ -1,34 +1,35 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 let users = [];
-export const getUsers = ((req,res)=>{
-    res.send(users);
-});
-export const getUserById =((req,res)=>{
-    const {id} = req.params;
-    const dataById = users.find((item)=>item.id === id);
-    res.send(dataById);
-});
-export const saveUser =((req,res)=>{
-    const request = req.body;
-    users.push({...request, id: uuid()});
-    res.send(users);
-});
+export const getUsers = (req, res) => {
+  res.send(users);
+};
+export const getUserById = (req, res) => {
+  const { id } = req.params;
+  const dataById = users.find((item) => item.id === id);
+  res.send(dataById);
+};
+export const saveUser = (req, res) => {
+  console.log("req,", req);
+  const request = req.body;
+  users.push({ ...request, id: uuid() });
+  res.send(users);
+};
 
-export const deleteUser =((req,res)=>{
-    const {id} = req.params;
-    
-    users = users.filter((item)=>{
-        return item.id !== id
-    });
-    
-    res.send(`users of id ${id} is deleted`);
-});
+export const deleteUser = (req, res) => {
+  const { id } = req.params;
 
-export const updateUser =((req,res)=>{
-    const user = users.find((item) => item.id === req.params.id);
-    user.name = req.body.name;
-    user.f_name = req.body.f_name;
-    user.m_name = req.body.m_name;
+  users = users.filter((item) => {
+    return item.id !== id;
+  });
 
-    res.send(users);
-});
+  res.send(`users of id ${id} is deleted`);
+};
+
+export const updateUser = (req, res) => {
+  const user = users.find((item) => item.id === req.params.id);
+  user.name = req.body.name;
+  user.f_name = req.body.f_name;
+  user.m_name = req.body.m_name;
+
+  res.send(users);
+};
